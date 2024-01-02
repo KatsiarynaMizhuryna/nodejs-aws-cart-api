1611203ed37b03e5daa182ba48639a13e8627879
Kate <ekaterina21051988@yandex.by>
2024-01-02 23:51:13 +0200

    feat: refactor: code refactor

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-# Build stage
 FROM node:20-alpine AS build
 WORKDIR /app
-COPY package*.json ./
-RUN npm install && npm cache clean --force
-COPY . .
+COPY --chown=node:node package*.json ./
+RUN npm ci --production && npm install rimraf --save-dev && npm cache clean --force
+COPY --chown=node:node . .
+USER node
 RUN npm run build
 
 
@@ -14,5 +14,6 @@ COPY --from=build /app/dist ./dist
 COPY package*.json ./
 RUN npm install --only=production && npm cache clean --force
 USER node
+ENV PORT=4000
 EXPOSE 4000
 CMD ["node", "dist/main.js"]
--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import serverlessExpress from '@vendia/serverless-express';
-import { Callback, Context, Handler } from 'aws-lambda';
-import 'dotenv/config';
 
 import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
-// const port = process.env.PORT || 4000;
-
-let server: Handler;
+const port = process.env.PORT || 4000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
- console.log('==============================')
-  app.enableCors({
-    origin: (req, callback) => callback(null, true),
-  });
-  app.use(helmet());
-
- // await app.listen(port);
-  
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress({ app: expressApp });
+    const app = await NestFactory.create(AppModule);
+    
+    app.enableCors({ origin: (req, callback) => callback(null, true), });
+    app.use(helmet());
+    
+    await app.listen(port);
 }
 bootstrap().then(() => {
-  console.log('App is running on %s port');
+    console.log('App is running on %s port', port);
 });
-
-export const handler: Handler = async (
-    event: any,
-    context: Context,
-    callback: Callback,
-) => {
-  
-  console.log(process.env);
-  
-  
-  server = server ?? (await bootstrap());
-  
-  console.log(`server`, server);
-  console.log(`DONE`);
-  console.log(`Event: ${JSON.stringify(event)}`);
-  console.log(`Event: ${event.body}`);
-  return server(event, context, callback);
-};
